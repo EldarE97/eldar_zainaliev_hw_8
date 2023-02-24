171ace1940dc0762f147fc95d265fe74e6bf2464
EldarE97 <master979797979797>
2023-02-24 22:07:54 +0300

    Initial commit

---
+++ src/kg/geecks/game/players/Magic.java
@@ -0,0 +1,11 @@
+package kg.geecks.game.players;
+
+public class Magic extends Hero {
+    public Magic(int health, int damage, String name) {
+        super(health, damage, SuperAbility.BOOST, name);
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+    }
+}
