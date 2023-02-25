5367a66331dd16082338d2e365602950ba46119d
EldarE97 <master979797979797>
2023-02-25 12:33:07 +0300

    hw_8

--- src/kg/geecks/game/players/Magic.java
+++ src/kg/geecks/game/players/Magic.java
@@ -1,11 +1,17 @@
 package kg.geecks.game.players;
 
 public class Magic extends Hero {
+    private int additionalAttack = 20;
     public Magic(int health, int damage, String name) {
         super(health, damage, SuperAbility.BOOST, name);
     }
 
     @Override
     public void applySuperPower(Boss boss, Hero[] heroes) {
+          for (int i = 0; i < heroes.length; i++) {
+              if(heroes[i].getHealth() > 0 && heroes[i] != this) {
+                  heroes[i].setDamage(heroes[i].getDamage() + this.additionalAttack);
+              }
+          } System.out.println("Www");
     }
 }
